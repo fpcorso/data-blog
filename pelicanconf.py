ac0e0d48dbe2d33fc9e808ff4331f64de638d6f7
Frank Corso <frank@frankcorso.me>
2020-06-01 21:54:41 -0400

    Tries to set up RSS

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,7 @@ DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None

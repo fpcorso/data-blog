b5db40214a1019a897c9091f4924c540ac900a70
Frank Corso <frank@frankcorso.me>
2020-06-01 21:08:21 -0400

    Wraps up most of the theme and templates

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,12 +7,9 @@ SITENAME = "Frank's Blog"
 SITEURL = ''
 THEME = 'theme'
 GRAVATAR = 'https://secure.gravatar.com/avatar/3c79ff484379feb07f575dadee9b3562?s=100'
-
 PATH = 'content'
-
 TIMEZONE = 'America/New_York'
 DEFAULT_DATE_FORMAT = '%B %d, %Y'
-
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -25,8 +22,11 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('Facebook', '#'),)
+# Navigation settings
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = True
+MENUITEMS = (('My Articles', SITEURL),
+             ('Topics', 'categories.html'))
 
 DEFAULT_PAGINATION = False
 

0ef3151e2c57d34b6733ea4cc068963ea91e685c
Frank Corso <frank@frankcorso.me>
2020-10-25 10:48:49 -0400

    Clean up conf files

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,17 +18,6 @@ TWITTER = 'https://twitter.com/fpcorso'
 GITHUB = 'https://github.com/fpcorso'
 INSTAGRAM = 'https://www.instagram.com/fpcorso/'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('You can modify those links in your config file', '#'),)
-
 # Navigation settings
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = True

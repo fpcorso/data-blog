5769af09da17c2472fc32c08f19aa65c9921adbc
Frank Corso <frank@frankcorso.me>
2020-11-01 11:18:33 -0500

    Remove scroll indicator

--- theme/static/js/site.js
+++ theme/static/js/site.js
@@ -52,34 +52,3 @@ document.querySelector( '#dark-switch' ).addEventListener( 'change', function( e
         saveDarkModePreference(false);
     }
 });
-
-// Sets up the scroll indicator.
-var p_elements = document.querySelectorAll('p');
-document.querySelector('#scroll-indicator').style.display = 'block';
-document.addEventListener('wheel', (evt) => {
-    var seen = 0;
-
-    // Determine which p elements are visible and calculate seen percentage.
-    p_elements.forEach((element, index) => {
-        let rect = element.getBoundingClientRect();
-        let inView = rect.bottom > 0 && rect.bottom < window.innerHeight;
-        if ( inView ) {
-            seen = ( index / p_elements.length ) * 100;
-        }
-    });
-
-    // If the first p is visible, set to 0.
-    let rect = p_elements[0].getBoundingClientRect();
-    let inView = rect.bottom > 0 && rect.bottom < window.innerHeight;
-    if ( inView ) {
-        seen = 0;
-    }
-
-    // If the last p is visible, set to 100.
-    rect = p_elements[p_elements.length - 1].getBoundingClientRect();
-    inView = rect.bottom > 0 && rect.bottom < window.innerHeight;
-    if ( inView ) {
-        seen = 100;
-    }
-    document.querySelector('#scroll-indicator').value = seen;
-}, { capture: false, passive: true})
